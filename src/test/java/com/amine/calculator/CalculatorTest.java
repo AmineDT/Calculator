c45bb6b5d2d6b6352e5a4577bf7ababac7fb0819
AmineDT <amine.elbakkouri@outlook.com>
2023-12-01 11:39:58 +0100

    Ajouter la logique de l’addition, controller et test unitaire

--- src/test/java/com/amine/calculator/CalculatorTest.java
+++ src/test/java/com/amine/calculator/CalculatorTest.java
@@ -1,11 +1,11 @@
 package com.amine.calculator;
 
-import junit.framework.*;
-
-
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
 
 public class CalculatorTest {
     private Calculator calculator = new Calculator();
+
     @Test
     public void testSum() {
         assertEquals(8, calculator.sum(3, 5));
